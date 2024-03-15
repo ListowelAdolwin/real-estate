be1ee4a544cdff56f79ab7a2d4fae7a22ff02a49
ListowelAdolwin <listoweladolwin@gmail.com>
2024-03-15 22:44:16 +0000

    Implemented login

--- api/controllers/authControllers.js
+++ api/controllers/authControllers.js
@@ -1,5 +1,7 @@
 const User = require('../models/User')
 const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+const handleErrors = require('../utils/errors')
 
 exports.Register = async (req, res, next) => {
     const {username, email, password} = req.body
@@ -12,9 +14,27 @@ exports.Register = async (req, res, next) => {
         console.log(error)
         next(error)
     }
-    
 }
 
-exports.Login = (req, res) => {
-    console.log(req.body)
+
+exports.Login = async (req, res, next) => {
+    const {email, password} = req.body
+
+    try {
+        const validUser = await User.findOne({email})
+    if (!validUser){
+        return next(handleErrors(404, "No user associated with this email"))
+    }
+
+    const validPassword = await bcrypt.compare(password, validUser.password)
+    if (!validPassword){
+        return next(handleErrors(401, "Invalid credentials"))
+    }
+    const token = jwt.sign({id: validUser._id}, process.env.JWT_SECRET_KEY, {expiresIn: '1d'})
+    const {password:pass, ...rest} = validUser._doc
+    res.cookie('accessToken', token, {httpOnly: true}).status(200).json(rest)
+    } catch (error) {
+        next(error)
+    }
+    
 }
