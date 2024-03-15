4b056267b951e4872a6849b38a98f0dd8e1682af
ListowelAdolwin <listoweladolwin@gmail.com>
2024-03-15 14:12:00 +0000

    Made navbar responsive

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,88 @@
+import React from "react";
+import { useState } from "react";
+import { FaSearch } from "react-icons/fa";
+import { Link } from "react-router-dom"; // Wherever this component is imported, it should wrapped by the BrowerRouter
+
+function Header() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleNavlinkClick = () => {
+    if (window.innerWidth <= 768) {
+      setIsOpen(false);
+    }
+  };
+
+  return (
+    <div>
+      <header
+        className={`${isOpen ? "block p-4 bg-violet-300 text-2xl space-y-4" : "flex justify-between gap-3 items-center p-4 bg-violet-300"}`}
+      >
+        <div className="">
+          <Link to="/">
+            <div className="flex flex-wrap">
+              <span className="">Real</span>
+              <span className="text-sky-500">Estate</span>
+            </div>
+          </Link>
+        </div>
+        <div className={`${isOpen ? "hidden" : "block"}`}>
+          <form
+            action=""
+            className="flex p-2 rounded bg-slate-200 items-center"
+          >
+            <input
+              className="bg-transparent focus:outline-none w-40 sm:w-64"
+              type="text"
+              name="q"
+              id=""
+              placeholder="Search real estates"
+            />
+            <FaSearch className="text-slate-500"></FaSearch>
+          </form>
+        </div>
+        <div>
+          <ul
+            className={`flex flex-grow flex-col ${isOpen ? "block" : "hidden"} space-y-4 sm:flex sm:flex-row sm:space-x-4 sm:space-y-0`}
+          >
+            <Link onClick={handleNavlinkClick} className="active" to="/">
+              <li>Home</li>
+            </Link>
+            <Link onClick={handleNavlinkClick} to="/login">
+              <li>Login</li>
+            </Link>
+            <Link onClick={handleNavlinkClick} to="/register">
+              <li>Register</li>
+            </Link>
+            <Link onClick={handleNavlinkClick} to="/about">
+              <li>About</li>
+            </Link>
+          </ul>
+        </div>
+
+        <div className="block sm:hidden">
+          <button
+            onClick={() => setIsOpen(!isOpen)}
+            className="flex items-center px-3 py-2 rounded text-black-500 hover:text-black-400"
+          >
+            <svg
+              className={`fill-current h-4 w-4 ${isOpen ? "hidden" : "block"}`}
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
+            </svg>
+            <svg
+              className={`fill-red-900 font-bold h-4 w-4 ${isOpen ? "block" : "hidden"}`}
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path d="M10 8.586L2.929 1.515 1.515 2.929 8.586 10l-7.071 7.071 1.414 1.414L10 11.414l7.071 7.071 1.414-1.414L11.414 10l7.071-7.071-1.414-1.414L10 8.586z" />
+            </svg>
+          </button>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default Header;
