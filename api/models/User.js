c0588b64d0c6afcbb6ebb143e3aad9c2abd60612
ListowelAdolwin <listoweladolwin@gmail.com>
2024-03-16 20:43:16 +0000

    Added avatar field to User model

--- api/models/User.js
+++ api/models/User.js
@@ -14,6 +14,10 @@ const UserSchema = new mongoose.Schema ({
     password: {
         type: String,
         required: true
+    },
+    avatar: {
+        type: String,
+        default: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fcommons.wikimedia.org%2Fwiki%2FFile%3AUser-avatar.svg&psig=AOvVaw2fmx2iGhgl7QbdpUmUqFTk&ust=1710703716482000&source=images&cd=vfe&opi=89978449&ved=0CBMQjRxqFwoTCMjZtozC-YQDFQAAAAAdAAAAABAE"
     }
 }, {timestamps: true});
 
