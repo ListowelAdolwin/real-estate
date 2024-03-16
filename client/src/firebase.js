2deff863b7b3b436ee11a6c723a95b1f02db1b44
ListowelAdolwin <listoweladolwin@gmail.com>
2024-03-16 20:42:22 +0000

    Integrated google oauth

---
+++ client/src/firebase.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  //apiKey: import.meta.env.VITE_FIREBASE_API_SECRET,
+  apiKey: "AIzaSyD_5-fgmyQCBucPhTvkD4yVtIDuH0NyiM0",
+  authDomain: "real-estate-listo.firebaseapp.com",
+  projectId: "real-estate-listo",
+  storageBucket: "real-estate-listo.appspot.com",
+  messagingSenderId: "667917258376",
+  appId: "1:667917258376:web:138069cac3600dd135193d"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
