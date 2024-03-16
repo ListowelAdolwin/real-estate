9fba717f698e0d21c03c297efc54d828b02508e1
ListowelAdolwin <listoweladolwin@gmail.com>
2024-03-16 15:31:22 +0000

    Implemented redux and redux persist

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import { useSelector } from 'react-redux';
 
 function Home() {
+  const {currentUser} = useSelector(state => state.user)
+  console.log("Current user: ", currentUser)
   return (
     <div>
-      <h1 className="underline">Home Page!</h1>
+      <h1 className="underline">Profile</h1>
+      <p>{currentUser.data.username}</p>
+      <p>{currentUser.data.email}</p>
     </div>
   );
 }
