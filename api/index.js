689291213c7827fcb2b0df18616809c2737dde32
ListowelAdolwin <listoweladolwin@gmail.com>
2024-03-17 19:57:40 +0000

    Implemented an updated profile endpoint

--- api/index.js
+++ api/index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 require('dotenv').config()
+const cookieParser = require('cookie-parser')
 const connectDB = require('./config/db')
 
 const userRoutes = require("./routes/userRoutes")
@@ -7,8 +8,9 @@ const authRoutes = require("./routes/authRoutes")
 
 const app = express()
 app.use(express.json())
+app.use(cookieParser())
 
-app.use("/api", userRoutes)
+app.use("/api/user", userRoutes)
 app.use("/api/auth", authRoutes)
 
 app.listen(3000, () => {
