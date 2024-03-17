53a580e160385e8b7cd32a24def49096c6e42947
ListowelAdolwin <listoweladolwin@gmail.com>
2024-03-17 19:57:29 +0000

    Implemented an updated profile endpoint

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -1,5 +1,31 @@
-exports.getListowel = (req, res) => {
-    res.json({"Message": "Hello, Listowel!"})
+const handleErrors = require("../utils/errors")
+const User = require("../models/User")
+const bcrypt = require('bcrypt')
+
+
+exports.updateUser = async (req, res, next) => {
+    const user = req.user
+    const userId = req.params.id
+    if (user.id != userId) {
+        return next(handleErrors(401, "You can only update your profile"))
+    }
+    if (req.body.password){
+        req.body.password = bcrypt.hashSync(req.body.password, 10)
+    }
+    try {
+        const updatedUser = await User.findByIdAndUpdate(userId, {
+            $set: {
+                username: req.body.username,
+                email: req.body.email,
+                password: req.body.password,
+                avatar: req.body.avatar
+            }
+        }, {new: true})
+        const {password:pass, ...rest} = updatedUser._doc
+        res.status(200).json(rest)
+    } catch (error) {
+        next(error)
+    }
 }
 
 exports.getAdolwin = (req, res) => {
