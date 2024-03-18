2e52f92ba55184733c8c0f642637be9e16497452
ListowelAdolwin <listoweladolwin@gmail.com>
2024-03-18 09:44:42 +0000

    Added avatar field

--- api/models/User.js
+++ api/models/User.js
@@ -17,7 +17,7 @@ const UserSchema = new mongoose.Schema ({
     },
     avatar: {
         type: String,
-        default: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fcommons.wikimedia.org%2Fwiki%2FFile%3AUser-avatar.svg&psig=AOvVaw2fmx2iGhgl7QbdpUmUqFTk&ust=1710703716482000&source=images&cd=vfe&opi=89978449&ved=0CBMQjRxqFwoTCMjZtozC-YQDFQAAAAAdAAAAABAE"
+        default: "https://firebasestorage.googleapis.com/v0/b/real-estate-listo.appspot.com/o/default_avatar.jpg?alt=media&token=fde2ea0a-8822-477f-b2d9-d8493452da88"
     }
 }, {timestamps: true});
 
