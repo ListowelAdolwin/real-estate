fb9bb7abc45a83b072d4f261fd66ffc87f968a83
ListowelAdolwin <listoweladolwin@gmail.com>
2024-03-18 09:45:18 +0000

    Added deleteUser endpoint

--- api/routes/userRoutes.js
+++ api/routes/userRoutes.js
@@ -1,13 +1,12 @@
 const express = require('express')
 
-const {updateUser, getAdolwin} = require("../controllers/userController")
+const {updateUser, deleteUser} = require("../controllers/userController")
 const {verifyToken} = require('../utils/verifyToken')
 
 const userRouter = express.Router()
 
-console.log("User routes")
-userRouter.get("/ado", getAdolwin)
 userRouter.post("/update/:id", verifyToken, updateUser)
+userRouter.delete("/delete/:id", verifyToken, deleteUser)
 
 
 module.exports = userRouter
