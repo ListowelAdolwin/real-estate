8db03cd6feb7a96fbff194be485ca2de5e178b37
ListowelAdolwin <listoweladolwin@gmail.com>
2024-03-18 09:39:22 +0000

    Updated routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,31 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import About from "./pages/About";
+import Header from "./components/Header";
+import PrivateRoute from "./components/PrivateRoute";
+import Profile from "./pages/Profile";
 
 function App() {
   return (
     <div>
       <BrowserRouter>
-      <Routes>
-        <Route path="/" element = {<Home />} />
-        <Route path="/about" element={ <About /> } />
-        <Route path="/register" element={ <Register />} />
-        <Route path="/login" element={ <Login /> } />
-      </Routes>
+        <Header />
+        <Routes>
+          <Route
+            path="/profile"
+            element={
+              <PrivateRoute>
+                <Profile />
+              </PrivateRoute>
+            }
+          />
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
       </BrowserRouter>
     </div>
   );
