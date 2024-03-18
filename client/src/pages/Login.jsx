abca78add3a639d0184ad465f34079039d5080b4
ListowelAdolwin <listoweladolwin@gmail.com>
2024-03-18 09:35:31 +0000

    Implemented google oauth

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -2,18 +2,23 @@ import React, { useState } from "react";
 import { Oval } from "react-loader-spinner";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
-import { registerFailure, registerStart, registerSuccess } from "../redux/features/user/userSlice";
+import {
+  registerFailure,
+  registerStart,
+  registerSuccess,
+} from "../redux/features/user/userSlice";
+import GoogleOauth from "../components/GoogleOauth";
 
 function Login() {
   const [userData, setUserData] = useState({});
 
-  const dispatch = useDispatch()
-  const { isLoading, errors } = useSelector(state => state.user)
+  const dispatch = useDispatch();
+  const { isLoading, errors } = useSelector((state) => state.user);
 
   const navigate = useNavigate();
 
   const handleChange = (e) => {
-    dispatch(registerFailure())
+    dispatch(registerFailure());
     setUserData({
       ...userData,
       [e.target.id]: e.target.value,
@@ -22,7 +27,7 @@ function Login() {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    dispatch(registerStart())
+    dispatch(registerStart());
     const res = await fetch("/api/auth/login", {
       method: "POST",
       headers: {
@@ -34,45 +39,45 @@ function Login() {
     console.log(data);
     if (res.ok) {
       navigate("/");
-      dispatch(registerSuccess({data}))
+      dispatch(registerSuccess({ data }));
     } else {
-      dispatch(registerFailure(data.message))
+      dispatch(registerFailure(data.message));
     }
   };
   return (
-    <div class="min-h-screen flex items-center justify-center w-full bg-gray-700">
-      <div class="bg-gray-500 shadow-md rounded-lg px-8 py-6 max-w-md">
-        <h1 class="text-2xl text-white font-bold text-center mb-4 dark:text-gray-200">
+    <div className="min-h-screen flex items-center justify-center w-full bg-gray-700">
+      <div className="bg-gray-500 shadow-md rounded-lg px-8 py-6 max-w-md">
+        <h1 className="text-2xl text-white font-bold text-center mb-4 dark:text-gray-200">
           Welcome Back!
         </h1>
         <form action="#">
-          <div class="mb-4">
+          <div className="mb-4">
             <label
               for="email"
-              class="block text-sm font-medium text-gray-700 dark:text-gray-300 mb-2"
+              className="block text-sm font-medium text-gray-700 dark:text-gray-300 mb-2"
             >
               Email
             </label>
             <input
               type="email"
               id="email"
-              class="shadow-sm bg-gray-300 rounded-md w-full px-3 py-2 border border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
+              className="shadow-sm bg-gray-300 rounded-md w-full px-3 py-2 border border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
               placeholder="Enter email"
               required
               onChange={handleChange}
             />
           </div>
-          <div class="mb-4">
+          <div className="mb-4">
             <label
               for="password"
-              class="block text-sm font-medium text-gray-700 dark:text-gray-300 mb-2"
+              className="block text-sm font-medium text-gray-700 dark:text-gray-300 mb-2"
             >
               Password
             </label>
             <input
               type="password"
               id="password"
-              class="shadow-sm bg-gray-300  rounded-md w-full px-3 py-2 border border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
+              className="shadow-sm bg-gray-300  rounded-md w-full px-3 py-2 border border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
               placeholder="Enter password"
               required
               onChange={handleChange}
@@ -80,28 +85,28 @@ function Login() {
           </div>
           <a
             href="#"
-            class="text-xs text-white hover:outline-3 hover:ring-2 hover:ring-offset-2 hover:ring-gray-500"
+            className="text-xs text-white hover:outline-3 hover:ring-2 hover:ring-offset-2 hover:ring-gray-500"
           >
             Forgot Password?
           </a>
-          <div class="flex items-center justify-between mt-3 mb-3">
-            <div class="flex items-center">
+          <div className="flex items-center justify-between mt-3 mb-3">
+            <div className="flex items-center">
               <input
                 type="checkbox"
                 id="remember"
-                class="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500 focus:outline-none"
-                checked
+                className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500 focus:outline-none"
+                defaultChecked
               />
               <label
                 for="remember"
-                class="ml-2 block text-sm text-gray-700 dark:text-gray-300"
+                className="ml-2 block text-sm text-gray-700 dark:text-gray-300"
               >
                 Remember me
               </label>
             </div>
             <a
               href="/register"
-              class="text-xs text-gray-800 hover:text-gray-900 hover:outline-3 hover:ring-2 hover:ring-offset-2 hover:ring-gray-500"
+              className="text-xs text-gray-800 hover:text-gray-900 hover:outline-3 hover:ring-2 hover:ring-offset-2 hover:ring-gray-500"
             >
               Create Account
             </a>
@@ -127,11 +132,12 @@ function Login() {
             <button
               onClick={handleSubmit}
               type="submit"
-              class="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-gray-700 hover:bg-gray-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+              className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-gray-700 hover:bg-gray-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
             >
               Login
             </button>
           )}
+          <GoogleOauth />
         </form>
       </div>
     </div>
