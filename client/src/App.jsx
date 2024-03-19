e5111a2f65bd055ebf7eb9f170478d5bc9dc424f
ListowelAdolwin <listoweladolwin@gmail.com>
2024-03-19 12:28:46 +0000

    Added listing route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,7 @@ import About from "./pages/About";
 import Header from "./components/Header";
 import PrivateRoute from "./components/PrivateRoute";
 import Profile from "./pages/Profile";
+import CreateListing from "./pages/CreateListing";
 
 function App() {
   return (
@@ -13,18 +14,14 @@ function App() {
       <BrowserRouter>
         <Header />
         <Routes>
-          <Route
-            path="/profile"
-            element={
-              <PrivateRoute>
-                <Profile />
-              </PrivateRoute>
-            }
-          />
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/register" element={<Register />} />
           <Route path="/login" element={<Login />} />
+          <Route element={<PrivateRoute />}>
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/create-listing" element={<CreateListing />} />
+          </Route>
         </Routes>
       </BrowserRouter>
     </div>
