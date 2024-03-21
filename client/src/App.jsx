19a12c13a52613566ab63d1a1f64a1fcf4eb3227
ListowelAdolwin <listoweladolwin@gmail.com>
2024-03-21 13:44:35 +0000

    Added routes for displaying and editing listings

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,6 +7,8 @@ import Header from "./components/Header";
 import PrivateRoute from "./components/PrivateRoute";
 import Profile from "./pages/Profile";
 import CreateListing from "./pages/CreateListing";
+import Listing from "./pages/Listing";
+import EditListing from "./pages/EditListing";
 
 function App() {
   return (
@@ -18,9 +20,11 @@ function App() {
           <Route path="/about" element={<About />} />
           <Route path="/register" element={<Register />} />
           <Route path="/login" element={<Login />} />
+          <Route path="/listing/:listingId" element={<Listing />} />
           <Route element={<PrivateRoute />}>
             <Route path="/profile" element={<Profile />} />
             <Route path="/create-listing" element={<CreateListing />} />
+            <Route path="/listing/edit/:listingId" element={<EditListing />} />
           </Route>
         </Routes>
       </BrowserRouter>
