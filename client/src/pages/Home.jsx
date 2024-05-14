78659cf380050a37441947d029975f43cc8753af
ListowelAdolwin <listoweladolwin@gmail.com>
2024-05-14 21:36:48 +0000

    Refactored with eslint

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,6 +1,5 @@
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import { Swiper, SwiperSlide } from "swiper/react";
 import { Navigation } from "swiper/modules";
 import SwiperCore from "swiper";
 import "swiper/css/bundle";
@@ -44,7 +43,7 @@ export default function Home() {
         const data = await res.json();
         setSaleListings(data);
       } catch (error) {
-        log(error);
+        console.log(error);
       }
     };
     fetchOfferListings();
@@ -80,7 +79,7 @@ export default function Home() {
               className="w-60 bg-cyan-950 text-cyan-400 border border-cyan-400 border-b-4 font-medium overflow-hidden relative px-4 py-2 rounded-md hover:brightness-150 hover:border-t-4 hover:border-b active:opacity-75 outline-none duration-300 group"
             >
               <span className="bg-cyan-400 shadow-cyan-400 absolute -top-[150%] left-0 rounded-md opacity-50 group-hover:top-[150%] duration-500 shadow-[0_0_10px_10px_rgba(0,0,0,0.3)]"></span>
-              Lets' Get Started
+              Let's Get Started
             </Link>
           </div>
         </div>
